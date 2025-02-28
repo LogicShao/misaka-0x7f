b9e69ab19f65a0e5110f67db414b659834df07ca
logicshao <1712207189@qq.com>
2025-03-01 02:24:36 +0800

    hello

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,21 @@ import { Context, Schema } from 'koishi'
 
 export const name = 'misaka-0x7f'
 
-export interface Config {}
+export interface Config { }
 
 export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
-  // write your plugin here
+  let self_info = 'I am misaka-0x7f, a simple bot'
+  ctx.command('whoru').action(({ session }) => {
+    session.send(self_info)
+  })
+
+  ctx.on('message', (session) => {
+    if (session.content === 'hello') {
+      session.send('hello')
+    } else if (session.content === 'how are you') {
+      session.send('I am fine')
+    }
+  })
 }
